3aa7d21f9cb2c9fb62123174feaaddac384d1c6f
Ethan Wong <87055387+ethanwongca@users.noreply.github.com>
2023-06-05 01:02:48 -0700

    Update ActionQueue.java

--- assignment2/src/assignment2/ActionQueue.java
+++ assignment2/src/assignment2/ActionQueue.java
@@ -2,15 +2,13 @@ package assignment2;
 //March 20 Ver
 public class ActionQueue extends MyQueue<Direction> {
     //following my previous implementation
-    //two stacks might come in handy like the art example in class
-    //so keep in track of the num and brackets
     public ActionQueue() {
         super();
     }
 
     public void clear() {
         super.clear();
-        //anything else lol
+  
     }
 
     public void loadFromEncodedString(String s) {
@@ -19,7 +17,7 @@ public class ActionQueue extends MyQueue<Direction> {
         MyStack<String> stackDirection = new MyStack<>();
         MyQueue<String> tempDirectionQueue = new MyQueue<>();
         MyQueue<String> finalQueue = new MyQueue<>();
-        //maybe while is more effective
+  
         String num = "";
         String direction = "";
         String temp;
@@ -30,10 +28,8 @@ public class ActionQueue extends MyQueue<Direction> {
         int finalCounter = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            //remove the initial cases
-            //forgot you note: Maybe change target Queue
             char c = s.charAt(i);
-            //first puts num as normal
+  
             if ((Character.isDigit(c))) {
                 num += c;
                 if((!(direction.equals(""))) && (!(brackets.isEmpty()))) {
@@ -61,39 +57,38 @@ public class ActionQueue extends MyQueue<Direction> {
                     finalQueue.enqueue("" + c);
                     finalCounter++;
                 } else {
-                    if(stackNum.isEmpty() || brackets.isEmpty()) { //for the case 2S[N] new edit
+                    if(stackNum.isEmpty() || brackets.isEmpty()) { //for the case 2S[N] 
                         throw new IllegalArgumentException("Wrong");
                     }
                     direction += c;
                 }
                 continue;
             }
-            // will finish tmrw has issue with direction logic.
-            if (c == ']') { //have to count brackets somehow
+        
+            if (c == ']') { 
                 if(!(direction.equals(""))) {
                     stackDirection.push(direction);
-                }//push whatever to trick if
+                }
                 if(direction.equals("") && stackDirection.isEmpty() && tempDirectionQueue.isEmpty()){
                     throw new IllegalArgumentException("Missing Direction");
-                } //check this
+                } 
                 numClosedBracket++;
                 if (stackNum.isEmpty() || brackets.isEmpty()) {
                     throw new IllegalArgumentException("Something is wrong");
                 }
-                //two cases either it is in brackets or not
-                //between the two we need to do this now continue with either or
+      
                 int coefficient = stackNum.pop();
 
                 brackets.pop();
                 if (!(direction.equals(""))) {
-                    //right in essence
-
+        
+        
                     temp = stackDirection.pop();
                     for (int n = 0; n < coefficient - 1; n++) {
-                        temp += direction; //fixed now not a multiple
-                    } // this for loop is wrong
+                        temp += direction; 
+                    } 
 
-                    //the 0 case, clear the temp and clear the direction
+             
                     if(coefficient == 0){
                         num = "";
                         direction = "";
@@ -102,21 +97,20 @@ public class ActionQueue extends MyQueue<Direction> {
                     }
 
 
-                    //currently we have it for 3[NE]2[S] but need to combine
-                    //put this in a queue instead so we can push out later
+              
                     if(brackets.isEmpty()){ //two situations whether it is empty or not
                         finalQueue.enqueue(temp);
                         finalCounter ++;
-                    } else if (coefficient != 0){ //changed to else if
+                    } else if (coefficient != 0){ 
                         tempDirectionQueue.enqueue(temp);
                         counterTemp++;
                     }
                     num = "";
                     direction = "";
                     continue;
-                } else { //if direction.equals("")
+                } else {
                     String combine = "";
-                    for(int l = 0; l < counterTemp; l++){ //issue with the counter
+                    for(int l = 0; l < counterTemp; l++){
                         combine += tempDirectionQueue.dequeue();
                     }
 
@@ -124,14 +118,14 @@ public class ActionQueue extends MyQueue<Direction> {
                     combine = "";
 
                     for (int n = 0; n < coefficient; n++) {
-                        combine += temp; //fixed now not a multiple
-                    } //error here
+                        combine += temp; 
+                    } 
                     counterTemp = 0;
                     if(brackets.isEmpty()) {
                         finalQueue.enqueue(combine);
                         finalCounter++;
                         continue;
-                    } else { //ADDED TODAY NEED TO CHECK
+                    } else { 
                         tempDirectionQueue.enqueue(combine);
                         counterTemp ++;
                         continue;
