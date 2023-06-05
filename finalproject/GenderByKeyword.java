48266ce6fa1d7bd5f6f7accc8ce81473fbf78bb6
Ethan Wong <87055387+ethanwongca@users.noreply.github.com>
2023-06-05 01:10:20 -0700

    Update GenderByKeyword.java

--- finalproject/GenderByKeyword.java
+++ finalproject/GenderByKeyword.java
@@ -12,20 +12,19 @@ public class GenderByKeyword extends DataAnalyzer {
 	public MyHashTable<String, Integer> getDistByKeyword(String keyword) {
 		String properKeyword = stringConverter(keyword);
 		MyHashTable<String, Integer> finalTable = (MyHashTable<String, Integer>) super.nestedHash.get(properKeyword);
-		// ADD YOUR CODE BELOW THIS
+
 		return finalTable;
-		//ADD YOUR CODE ABOVE THIS
+
 	}
 
 	@Override
 	public void extractInformation() {
-		// ADD YOUR CODE BELOW THIS
-		//same code as prof just modified with gender
+
 		int genderIndex = super.parser.fields.get("gender");
 		int commentsIndex = super.parser.fields.get("comments");
-		//ADD YOUR CODE ABOVE THIS
+
 		for(int i = 0; i < super.parser.data.size(); i++) {
-			//Makes a non-duplicate arraylist
+
 			String comment = stringConverter(super.parser.data.get(i)[commentsIndex]);
 			comment = comment.replaceAll("[^a-z']+", " ");
 			String gender = super.parser.data.get(i)[genderIndex];
